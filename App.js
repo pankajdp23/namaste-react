35c39bc0f17b7aa1039791495a3c2be86573f392
pankajdp23 <pankaj.patil@excellarate.com>
2025-02-22 08:42:48 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
